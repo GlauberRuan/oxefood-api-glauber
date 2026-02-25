e6fd8f5b8051b0ed4f9094eeb3aa9d0ffdb281bd
GlauberRuan <grfs@discente.ifpe.edu.br>
2026-02-25 00:29:59 -0300

    enviando arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiGlauberApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiGlauberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiGlauberApplication.class, args);
+	}
+
+}
